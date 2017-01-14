ccef6d110a542b5d29311cc2359371a2b5610c95
theDataGeek <wongman18@gmail.com>
2017-01-14 10:23:32 +0530

    template for adding file share and bcp

--- addFileShare.ps1
+++ addFileShare.ps1
@@ -1,5 +1,5 @@
 ﻿# persist credentials of file share to trusted list
-cmdkey /add:labdiag870.file.core.windows.net /user:labdiag870 /pass:8gnUh5/KtErq3sBF8wE+gRQllAaRGxGO74oiKtna2uTht+bTfmIYr3outuAiHSg0TY+oZBce3lNH/N0qct+M+A==
+cmdkey /add:<fileShareName>.file.core.windows.net /user:<fileShareName> /pass:<passCode>
 
 #connect to the file share
 net use z: \\labdiag870.file.core.windows.net\ubuntu-ssrs
